fe699bc543b679a96bfd81a5878c9b85aefda682
ggoggori <whgurwns2003@naver.com>
2022-06-17 00:12:33 +0900

    UPDATE: Import 수정

--- 이코테/5. DFS, BFS/5.10 음료수 얼려 먹기.py
+++ 이코테/5. DFS, BFS/5.10 음료수 얼려 먹기.py
@@ -2,6 +2,7 @@
 해설은 dfs로 풀었다.
 나는 bfs로 풀었는데, 코드가 너무 길고 깔끔하지 못한 것 같다.
 """
+from collections import deque
 
 n, m = map(int, input().split())
 
@@ -9,8 +10,9 @@ array = []
 for i in range(n):
     array.append(list(map(int, input())))
 
-dx = [0,0,-1,1]
-dy = [-1,1,0,0]
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
 
 def bfs(array, start):
     queue = deque([start])
@@ -18,19 +20,24 @@ def bfs(array, start):
     while queue:
         v = queue.popleft()
         x, y = v[0], v[1]
-        target = [(x+i,y+j) for i,j in zip(dx,dy) if 0<=x+i<len(array) and 0<=y+j<len(array[0])]
+        target = [
+            (x + i, y + j)
+            for i, j in zip(dx, dy)
+            if 0 <= x + i < len(array) and 0 <= y + j < len(array[0])
+        ]
         # 상하좌우로 가능한 좌표 찾기.
-        for i,j in target:
+        for i, j in target:
             if not array[i][j]:
                 array[i][j] = 1
-                queue.append((i,j))
+                queue.append((i, j))
     return array
+
 
 count = 0
 for i in range(n):
     for j in range(m):
         if array[i][j] == 0:
-            array = bfs(array,(i,j))
+            array = bfs(array, (i, j))
             count += 1
 
 print(count)
