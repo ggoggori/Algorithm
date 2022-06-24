9dabee3e74f6060fc515379e77657ac5c2258b70
ggoggori <whgurwns2003@naver.com>
2022-06-24 15:20:50 +0900

    ADD: 개선된 다익스트라

---
+++ 이코테/9. 최단 경로/9-2. 개선된 다익스트라.py
@@ -0,0 +1,43 @@
+import heapq
+
+INF = int(1e9)
+n, m = 6, 11
+start = 1
+graph = [
+    [],
+    [(2, 2), (3, 5), (4, 1)],
+    [(3, 3), (4, 2)],
+    [(2, 3), (6, 5)],
+    [(3, 3), (5, 1)],
+    [(3, 1), (6, 2)],
+    [],
+]
+visited = [False] * (n + 1)
+distance = [INF] * (n + 1)
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:  # 이미 처리된 최단거리이면 무시
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
+
