7e30bfac1c614f200be2fc10fa2d8df8dec574c8
ggoggori <whgurwns2003@naver.com>
2022-07-23 15:27:36 +0900

    ADD: 그리디 문제풀기

---
+++ 이코테/맞았지만 정답풀이와 다른 문제/4. 만들 수 없는 금액.py
@@ -0,0 +1,16 @@
+# 푼 횟수 : v
+
+# 내가 푼 풀이
+# from itertools import combinations
+
+# n = 5
+# array = [3, 2, 1, 1, 9]
+
+# result = set()
+
+# for i in range(1, n + 1):
+#     for items in list(combinations(array, i)):
+#         result.add(sum(items))
+
+# answer = set([i for i in range(1, max(result) + 1)])
+# min(answer - result)
